c33d962344d23e8cc25eff4892c6e2d5c798df75
lesujuca <lesujuca@gmail.com>
2017-03-01 07:47:42 -0400

    Clase XXXIII - Ensayos y división con la Clase XXXII

--- Clase XXXII/js/main.js
+++ Clase XXXII/js/main.js
@@ -7,64 +7,21 @@ $(document).ready(function(){
 	$("form").on("submit", function(event){
 		event.preventDefault();
 		
-		// Una validación antes de enviar un formulario
-		if(procesarValidacion($("input:radio[name=idioma]:checked"), 1) == 0)
-			return;
-		if(procesarValidacion($("#acepto"), 2) == 0)
-			return;
-
-		var str = $("#form1").serialize();
 		// Realizamos la peticion
-		$.ajax("js/ajax-no-cors.php", {
+		$.ajax("php/ajax.php", {
 			type: 'POST',
 			dataType: "json",
-			data: str
+			data: {
+				'nombre': $("#nombre").val(),
+				'mensaje': $("#mensaje").val()
+			}
 		})
 		.done(function(data, status, respuesta){
 				// console.info(data);
-				$(".contenido").children("ol").prepend($("<li>" + "<strong>" + data.nombre + "</strong> - Idioma: " + data.idioma + "<small> - " + data.fecha.mday + "/" + data.fecha.mon + "/" + data.fecha.year + " " + data.fecha.hours + ":" + data.fecha.minutes + ":" + data.fecha.seconds + "</small><br />" + "<span>" + data.mensaje + "</span>" + "<br />" + data.acepto + "</li>"));
+				$(".contenido").children("ol").prepend($("<li>" + "<strong>" + data.nombre + "</strong> - "  + "<small>" + data.fecha.mday + "/" + data.fecha.mon + "/" + data.fecha.year + " " + data.fecha.hours + ":" + data.fecha.minutes + ":" + data.fecha.seconds + "</small><br />" + "<span>" + data.mensaje + "</span>" + "</li>"));
 		})
 		.fail(function(respuesta, status, mensaje){
 				console.error(mensaje);
 		});
 	});
-
-	// Pruebas con el campo radio
-	$("input:radio[name=idioma]").on("change", function(){
-		// console.info($(this));
-		procesarValidacion($(this), 1);
-	});
-
-	// Pruebas con el campo checkbox
-	$("#acepto").on("change", function(){
-		// console.info($(this));
-		procesarValidacion($(this), 2);
-	});
-
-	function procesarValidacion(elemento, tipo){
-		// console.log(elemento);
-
-		if(tipo == 2){
-			if(elemento.prop('checked')){
-			    // console.info('Seleccionado');
-			    return 1;
-			}else{
-				console.warn('No Seleccionado');
-				return 0;
-			}
-		}
-
-		if(tipo == 1 || tipo == 3){
-			if(elemento.is(':checked')){
-				/*if(tipo == 1)
-		        	console.info('Campo correcto: ' + $('input:radio[name=idioma]:checked').val());
-		        else
-		        	console.info('Campo correcto');*/
-		        return 1;
-		    }else{
-		        console.warn('Se debe seleccionar un idioma');
-		        return 0;
-		    }
-		}
-	}
 });
